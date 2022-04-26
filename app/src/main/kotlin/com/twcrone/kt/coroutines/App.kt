2d4807c2b35dc7afaff9efa02450442bbbe7fa0e
Todd W Crone <twcrone@gmail.com>
2022-04-26 16:25:53 -0400

    Asynchronous timeout and resources

--- app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
+++ app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
@@ -2,49 +2,26 @@ package com.twcrone.kt.coroutines
 
 import kotlinx.coroutines.*
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
+var acquired = 0
+
+class Resource {
+    init { acquired++}
+    fun close() { acquired-- }
 }
 
-fun main() = runBlocking {
-    //launch { doEet() }
-//    val job = launch {
-//        repeat(1000) {
-//            println("job: I'm sleeping $it ...")
-//            delay(500L)
-//        }
-//    }
-//    delay(1300L)
-//    println("Canceling...")
-//    job.cancel()
-//    job.join()
-//    println("Done")
-    val start = System.currentTimeMillis()
-    val job = launch(Dispatchers.Default) {
-        var nextPrintTime = start
-        var i = 0
-        try {
-            repeat(1000) { i ->
-                println("job: sleeping $i ...")
-                delay(500L)
+fun main() {
+    runBlocking {
+        repeat(100_000) {
+            launch {
+                val resource = withTimeout(60) {
+                    delay(50)
+                    Resource()
+                }
+                resource.close()
             }
-        } finally {
-            println("job: finally")
         }
-//        while (isActive) {
-//            if(System.currentTimeMillis() >= nextPrintTime) {
-//                println("job: I'm sleeping ${i++} ...")
-//                nextPrintTime += 500L
-//            }
-//        }
     }
-    delay(1300L)
-    println("Trying to cancel...")
-    job.cancelAndJoin()
-    println("Done")
+    println(acquired)
 }
 
 suspend fun repeatLaunch() = coroutineScope {
