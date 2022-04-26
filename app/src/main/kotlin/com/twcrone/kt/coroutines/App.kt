cc4f71d3c8ab7e4162ad78345e143b05cf5b0d5e
Todd W Crone <twcrone@gmail.com>
2022-04-26 11:18:40 -0400

    Extract suspend function

--- app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
+++ app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
@@ -15,9 +15,11 @@ class App {
 }
 
 fun main() = runBlocking { // this: CoroutineScope
-    launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
-    }
-    println("Hello") // main coroutine continues while a previous one is delayed
+    launch { eet() }
+    println("Do") // main coroutine continues while a previous one is delayed
+}
+
+suspend fun eet() {
+    delay(1000L)
+    println("Eeet!!!")
 }
