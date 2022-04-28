bb1f38f5a3715d26e9cd29f686c2076cda426123
Todd W Crone <twcrone@gmail.com>
2022-04-28 17:17:29 -0400

    Threads vs Coroutines

--- app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
+++ app/src/main/kotlin/com/twcrone/kt/coroutines/App.kt
@@ -1,6 +1,8 @@
 package com.twcrone.kt.coroutines
 
 import kotlinx.coroutines.*
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 var acquired = 0
@@ -11,30 +13,25 @@ class Resource {
 }
 
 fun main() {
-    runBlocking {
-        val time = measureTimeMillis {
-            val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
-            val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
-            one.start()
-            two.start()
-            println("The answer is ${one.await() + two.await()}")
-        }
-        println("Completed in $time ms")
-//        repeat(100_000) {
-//            launch {
-//                var resource: Resource? = null
-//                try {
-//                    withTimeout(60) {
-//                        delay(50)
-//                        resource = Resource()
-//                    }
-//                } finally {
-//                    resource?.close()
-//                }
-//            }
-//        }
+
+    GlobalScope.launch {
+        delay(1000)
+        print("World")
     }
-    //println(acquired)
+
+    print("Hello, ")
+    sleep(1500)
+}
+
+fun old_main() {
+
+    thread {
+        sleep(1000)
+        println("World")
+    }
+
+    print("Hello, ")
+    sleep(1500)
 }
 
 suspend fun repeatLaunch() = coroutineScope {
